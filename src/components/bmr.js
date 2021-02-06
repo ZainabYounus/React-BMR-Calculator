e07599d7835d51e0f7b693ed3fd2b525ca4d55bc
= <=>
2021-02-06 20:29:36 +0500

    Added Both Imperial and Metric Calculations

--- src/components/bmr.js
+++ src/components/bmr.js
@@ -13,7 +13,9 @@ class bmr extends Component {
             activity: '',
             bmr: '',
             error: '',
-            calories: ''
+            calories: '',
+            calculationType: '',
+            heightCm: ''
         }
     }
 
@@ -53,11 +55,20 @@ class bmr extends Component {
         })
     }
 
+    handleCalculationChange = (event) => {
+        this.setState({ calculationType: event.target.value });
+    }
+
+    handleHeightCmChange = (event) => {
+        this.setState({ heightCm: event.target.value });
+    }
+
     calculateBMR() {
         let age = this.state.age;
         let gender = this.state.gender;
         let heightFeet = this.state.heightFeet;
         let heightInches = this.state.heightInches;
+        let heightCenti = this.state.heightCm;
         let weight = this.state.weight;
 
         if (age == '' || gender == '' || heightFeet == '' || heightInches == '' || weight == '') {
@@ -65,21 +76,41 @@ class bmr extends Component {
             return;
         }
 
-        /* BMR calculation (imperial): 
-         Man BMR = 66 + ( 6.2 × weight in pounds ) + ( 12.7 × height in inches ) – ( 6.76 × age in years )
-         Woman BMR = 655.1 + ( 4.35 × weight in pounds ) + ( 4.7 × height in inches ) - ( 4.7 × age in years )
-        */
-        let bmrCalc = '';
-        let height = ((heightFeet * 30.48) + (heightInches * 2.54));
+        if (this.state.calculationType == "imperial") {
+            /* BMR calculation (imperial): 
+            Man BMR = 66 + ( 6.2 × weight in pounds ) + ( 12.7 × height in inches ) – ( 6.76 × age in years )
+            Woman BMR = 655.1 + ( 4.35 × weight in pounds ) + ( 4.7 × height in inches ) - ( 4.7 × age in years )
+            */
+            let bmrCalc = '';
+            let height = ((heightFeet * 30.48) + (heightInches * 2.54));
 
-        if (gender == 2) {
-            //Calculation of Male BMR
-            bmrCalc = 66 + (6.2 * weight) + (12.7 * height) - (6.76 * age);
-        } else if (gender == 1) {
-            //Calculation of Female BMR
-            bmrCalc = 655.1 + (4.35 * weight) + (4.7 * height) - (4.7 * age);
+            if (gender == 2) {
+                //Calculation of Male BMR
+                bmrCalc = 66 + (6.2 * weight) + (12.7 * height) - (6.76 * age);
+            } else if (gender == 1) {
+                //Calculation of Female BMR
+                bmrCalc = 655.1 + (4.35 * weight) + (4.7 * height) - (4.7 * age);
+            }
+            this.setState({ bmr: bmrCalc });
+
         }
-        this.setState({ bmr: bmrCalc });
+        else if (this.state.calculationType == "metric") {
+            /* BMR calculation (Metric): 
+            Man BMR = 66.5 + ( 13.75 × weight in kg ) + ( 5.003 × height in cm ) – ( 6.755 × age in years )
+            Woman BMR = BMR = 655 + ( 9.563 × weight in kg ) + ( 1.850 × height in cm ) – ( 4.676 × age in years )
+            */
+            let bmrCalc = '';
+
+            if (gender == 2) {
+                //Calculation of Male BMR
+                bmrCalc = 66.5 + (13.5 * weight) + (12.7 * heightCenti) - (6.755 * age);
+            } else if (gender == 1) {
+                //Calculation of Female BMR
+                bmrCalc = 655 + (9.563 * weight) + (1.850 * heightCenti) - (4.676 * age);
+            }
+            this.setState({ bmr: bmrCalc });
+
+        }
 
         this.setState({ error: '' })
     }
@@ -87,10 +118,10 @@ class bmr extends Component {
     calculateCalories() {
         /*
         1. Little to no exercise	Daily kilocalories needed = BMR x 1.2
-2. Light exercise (1–3 days per week)	Daily kilocalories needed = BMR x 1.375
-3. Moderate exercise (3–5 days per week)	Daily kilocalories needed = BMR x 1.55
-4. Heavy exercise (6–7 days per week)	Daily kilocalories needed = BMR x 1.725
-5. Very heavy exercise (twice per day, extra heavy workouts)	Daily kilocalories needed = BMR x 1.9
+        2. Light exercise (1–3 days per week)	Daily kilocalories needed = BMR x 1.375
+        3. Moderate exercise (3–5 days per week)	Daily kilocalories needed = BMR x 1.55
+        4. Heavy exercise (6–7 days per week)	Daily kilocalories needed = BMR x 1.725
+        5. Very heavy exercise (twice per day, extra heavy workouts)	Daily kilocalories needed = BMR x 1.9
         */
 
         let workout = this.state.activity;
@@ -149,30 +180,75 @@ class bmr extends Component {
             resultCalories = <div className="result">{this.state.calories}</div>
         }
 
+        let imperialForm;
+        let metricForm;
+
+        if (this.state.calculationType == "imperial") {
+            imperialForm = <div>
+                <h2>BMR &amp; Daily Calorie Calculator</h2>
+                {error}
+                <div className="inputwrap">
+                    <label className="label">Gender</label>
+                    <label><input type="radio" className="genderF" chechked={this.state.gender === "1"} onChange={this.handleGenderChange} name="gender" value="1" />Female</label>
+                    <label><input type="radio" className="genderM" chechked={this.state.gender === "2"} onChange={this.handleGenderChange} name="gender" value="2" />Male</label>
+                </div>
+                <div className="inputwrap">
+                    <label className="label">Weight in Pounds</label>
+                    <input type="number" value={this.state.weight} onChange={this.handleWeightChange} name="weight" className="weight" min="0" max="999" />
+                </div>
+                <div className="inputwrap">
+                    <label className="label">Height in feet and inches</label>
+                    <input type="number" value={this.state.heightFeet} onChange={this.handleHeightFeetChange} name="heightFeet" className="heightFeet" min="0" max="8" />
+                    <input type="number" value={this.state.heightInches} onChange={this.handleHeightInchesChange} name="heightInches" className="heightInches" min="0" max="11" />
+                </div>
+                <div className="inputwrap">
+                    <label className="label">Age in years</label>
+                    <input type="number" value={this.state.age} onChange={this.handleAgeChange} className="age" name="age" min="0" max="120" />
+                </div>
+                <button type="button" onClick={() => this.calculateBMR()}>Calculate BMR</button>
+            </div>
+
+        }
+        else if (this.state.calculationType == "metric") {
+            metricForm = <div>
+                <h2>BMR &amp; Daily Calorie Calculator</h2>
+                {error}
+                <div className="inputwrap">
+                    <label className="label">Gender</label>
+                    <label><input type="radio" className="genderF" chechked={this.state.gender === "1"} onChange={this.handleGenderChange} name="gender" value="1" />Female</label>
+                    <label><input type="radio" className="genderM" chechked={this.state.gender === "2"} onChange={this.handleGenderChange} name="gender" value="2" />Male</label>
+                </div>
+                <div className="inputwrap">
+                    <label className="label">Weight in Kg</label>
+                    <input type="number" value={this.state.weight} onChange={this.handleWeightChange} name="weight" className="weight" min="0" max="999" />
+                </div>
+                <div className="inputwrap">
+                    <label className="label">Height in Cm</label>
+                    <input type="number" value={this.state.heightCm} onChange={this.handleHeightCmChange} name="heightCm" className="heightCm" min="0" />
+                </div>
+                <div className="inputwrap">
+                    <label className="label">Age in years</label>
+                    <input type="number" value={this.state.age} onChange={this.handleAgeChange} className="age" name="age" min="0" max="120" />
+                </div>
+                <button type="button" onClick={() => this.calculateBMR()}>Calculate BMR</button>
+            </div>
+
+        }
+
         return (
             <div id="bmrcalc">
+
                 <div className="form">
-                    <h2>BMR &amp; Daily Calorie Calculator</h2>
-                    {error}
+
+                    <h2>Choose Calculation Method</h2>
                     <div className="inputwrap">
-                        <label className="label">Gender</label>
-                        <label><input type="radio" className="genderF" chechked={this.state.gender === "1"} onChange={this.handleGenderChange} name="gender" value="1" />Female</label>
-                        <label><input type="radio" className="genderM" chechked={this.state.gender === "2"} onChange={this.handleGenderChange} name="gender" value="2" />Male</label>
+                        <label className="label">Calculation Type</label>
+                        <label><input type="radio" chechked={this.state.calculationType === "imperial"} onChange={this.handleCalculationChange} name="calcType" value="imperial" />Imperial</label>
+                        <label><input type="radio" chechked={this.state.calculationType === "metric"} onChange={this.handleCalculationChange} name="calcType" value="metric" />Metric</label>
                     </div>
-                    <div className="inputwrap">
-                        <label className="label">Weight in Pounds</label>
-                        <input type="number" value={this.state.weight} onChange={this.handleWeightChange} name="weight" className="weight" min="0" max="999" />
-                    </div>
-                    <div className="inputwrap">
-                        <label className="label">Height in feet and inches</label>
-                        <input type="number" value={this.state.heightFeet} onChange={this.handleHeightFeetChange} name="heightFeet" className="heightFeet" min="0" max="8" />
-                        <input type="number" value={this.state.heightInches} onChange={this.handleHeightInchesChange} name="heightInches" className="heightInches" min="0" max="11" />
-                    </div>
-                    <div className="inputwrap">
-                        <label className="label">Age in years</label>
-                        <input type="number" value={this.state.age} onChange={this.handleAgeChange} className="age" name="age" min="0" max="120" />
-                    </div>
-                    <button type="button" onClick={() => this.calculateBMR()}>Calculate BMR</button>
+
+                    {imperialForm}
+                    {metricForm}
                     {resultBMR}
                     {showCaloriesCalc}
                     {resultCalories}
